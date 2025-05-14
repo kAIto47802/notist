2f74d9d52f921b00a5d300cae623063dba138ad1
kAIto47802 <115693559+kAIto47802@users.noreply.github.com>
2025-05-14 09:35:56 +0900

    Update import

--- notifyme/__init__.py
+++ notifyme/__init__.py
@@ -1,9 +1,13 @@
+from notifyme._core import init, send, watch
 from notifyme._discord import DiscordNotifier
 from notifyme._slack import SlackNotifier
 
 __all__ = [
     "DiscordNotifier",
     "SlackNotifier",
+    "init",
+    "send",
+    "watch",
 ]
 
 __version__ = "0.1.0"
