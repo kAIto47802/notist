7cc1d0106bc0cbdecf5f7bde879cdcdb62d47fc6
kAIto47802 <115693559+kAIto47802@users.noreply.github.com>
2025-07-23 15:11:09 +0900

    Rename

--- notifystate/_notifiers/__init__.py
+++ notifystate/_notifiers/__init__.py
@@ -1,7 +1,7 @@
-from notifyend._notifiers.base import BaseNotifier
-from notifyend._notifiers.core import init, send, watch
-from notifyend._notifiers.discord import DiscordNotifier
-from notifyend._notifiers.slack import SlackNotifier
+from notifystate._notifiers.base import BaseNotifier
+from notifystate._notifiers.core import init, send, watch
+from notifystate._notifiers.discord import DiscordNotifier
+from notifystate._notifiers.slack import SlackNotifier
 
 __all__ = [
     "BaseNotifier",
