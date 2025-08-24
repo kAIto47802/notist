721ac9a3dd107692218e07f7d936d04434dcead7
kAIto47802 <115693559+kAIto47802@users.noreply.github.com>
2025-08-25 02:42:00 +0900

    Update

--- notist/__init__.py
+++ notist/__init__.py
@@ -1,18 +1,17 @@
-from notist._core import init, register, send, watch
+from notist._core import init, register, send, watch, watch_iterable
 from notist._notifiers import (
-    BaseNotifier,
     DiscordNotifier,
     SlackNotifier,
 )
 
 __all__ = [
-    "BaseNotifier",
     "DiscordNotifier",
     "SlackNotifier",
     "init",
     "register",
     "send",
     "watch",
+    "watch_iterable",
 ]
 
 __version__ = "0.1.0dev"
